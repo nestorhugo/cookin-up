a57cd20afcbe9ee0a795d46ce11cffc0aaddb83a
nestorhugo <nestorhugo@proton.me>
2024-01-30 20:46:16 -0300

    Add Banner component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+export default {
+  components: { Banner },
+};
+</script>
+
 <template>
-  <h1>Meu primeiro projeto Vue!</h1>
+  <Banner />
 </template>
